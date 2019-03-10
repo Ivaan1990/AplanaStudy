1c05da05af4ca5f972a732c4b065935cae48307b
Ivaan1990 <ivaan7845@gmail.com>
2019-03-10 21:58:11 +0300

    Task3

--- src/ThirdTask/WordWriter.java
+++ src/ThirdTask/WordWriter.java
@@ -2,27 +2,55 @@ package ThirdTask;
 
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
+import java.util.*;
 
 /**
  * @author Ivan Yushin
  * Для тестирования работы программы, укажите путь к вашему файлу в параметра объекта writer
  * @see #writeWordsIntoFile(FileWriter)
  * @see FileWriter
- * @see #listOfAllWorld намеренно установил общую вместимость списка для повышения производительности
+ * TODO @see #listOfAllWorld намеренно установил общую вместимость списка для повышения производительности
  */
-public class WordWriter {
-
-    private static String[] words ={"Ы", "Ёж", "Суп", "Кадр", "Привет", "Выход", "Квадрат", "Персонал", "Аукционер", "Арифметика"};
-    private static List<String> listOfAllWorld = new ArrayList<>(10000);
+public class WordWriter implements Comparable<WordWriter>{
+    private static String[] words ={"Ы", "Яд", "Суп", "Кадр", "Привет", "Выход", "Квадрат", "Персонал", "Аукционер", "Арифметика"};
+    private static List<String> listOfAllWorld = new ArrayList<>();
+    private static Map<String, Integer> map = new TreeMap<String, Integer>();
 
     public static void main(String[] args) throws IOException {
         FileWriter writer = new FileWriter("C:\\Users\\Asus\\Desktop\\testExample.txt");
 
-        /** записываем данные в файл*/
+        /** записываем данные в файл */
         writeWordsIntoFile(writer);
+
+        /** Сортируем */
+        //TODO возможно требуется перезапись отсортированных слов в файл
+        Collections.sort(listOfAllWorld);
+
+        /** @see map Подсчет повторений, сбор информации в словарь */
+        for(String word : words){
+            int countOfWords = Collections.frequency(listOfAllWorld, word);
+            map.put(word, countOfWords);
+        }
+
+        /** Вывод в консоль статистики повторений слов */
+        for(Map.Entry<String, Integer> pair : map.entrySet()){
+            System.out.println("Cлово: " + pair.getKey() + ", повторений " + pair.getValue());
+        }
+
+        /** Поиск слова с максимальным количеством повторений */
+        findMax();
+    }
+
+    /**
+     * @see #findMax()
+     */
+    private static void findMax() {
+        int max = Collections.max(map.values());
+        for(Map.Entry<String, Integer> pair : map.entrySet()){
+            if (pair.getValue() == max){
+                System.out.println("\nМаксимум повторов у " + pair.getKey() + "\nВстречается в файле " + pair.getValue());
+            }
+        }
     }
 
     /**
@@ -35,11 +63,17 @@ public class WordWriter {
         StringBuilder builder = new StringBuilder();
         for (int i = 0; i < 10000; i++) {
             Random random = new Random();
-            int randumIndex = random.nextInt(words.length);
-            builder.append(words[randumIndex]).append(" ");
-            listOfAllWorld.add(words[randumIndex]);
+            int randomIndex = random.nextInt(words.length);
+            builder.append(words[randomIndex]).append(" ");
+            listOfAllWorld.add(words[randomIndex]);
         }
         writer.write(builder.toString().trim());
         writer.close();
+    }
+
+    @Override
+    public int compareTo(WordWriter o) {
+
+        return 0;
     }
 }
